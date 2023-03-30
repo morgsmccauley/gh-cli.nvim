ab81ca38e4ebfcf045975cdf12b9d0e17c5204cb
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-30 15:00:38 +1300

    feat: Create stubbed `GH` user command

--- plugin/gh.lua
+++ plugin/gh.lua
@@ -1,1 +1,12 @@
+local function gh_command_handler(args)
+  print("Command passed: " .. args.args)
+end
 
+local function gh_completion()
+  return {}
+end
+
+vim.api.nvim_create_user_command("GH", gh_command_handler, {
+  nargs = '*',
+  complete = gh_completion
+})
