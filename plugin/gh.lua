c9eac1b729aaa417102cd18eacf0961a26f0869f
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-30 15:01:56 +1300

    feat: Use `gh __completion` to autocomplete top level commands

--- plugin/gh.lua
+++ plugin/gh.lua
@@ -3,7 +3,14 @@ local function gh_command_handler(args)
 end
 
 local function gh_completion()
-  return {}
+  local command_output = io.popen("gh __complete \"\"")
+  local output_lines = {}
+  for line in command_output:lines() do
+    table.insert(output_lines, line)
+  end
+  command_output:close()
+
+  return output_lines
 end
 
 vim.api.nvim_create_user_command("GH", gh_command_handler, {
