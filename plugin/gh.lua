5a50e6e4043baad52de33d4ebb189d0032ea1ab6
Morgan McCauley <morgan@mccauley.co.nz>
2023-03-31 20:44:19 +1300

    feat: Autocomplete reviewers

--- plugin/gh.lua
+++ plugin/gh.lua
@@ -4,6 +4,38 @@ local function gh_command_handler(args)
   print("Command passed: " .. args.args)
 end
 
+local function fetch_reviewers()
+  local query = [[
+    {
+      organization(login: "near") {
+        membersWithRole(first:10) {
+          nodes {
+            login
+          }
+        }
+      }
+    }
+  ]]
+
+  local reviewers = {}
+
+  Job:new({
+    command = 'gh',
+    args = {
+      'api',
+      'graphql',
+      '-f',
+      'query=' .. query,
+      '--jq=.data.organization.membersWithRole.nodes[].login'
+    },
+    on_stdout = function(_err, data)
+      table.insert(reviewers, data)
+    end,
+  }):sync()
+
+  return reviewers
+end
+
 local function gh_completion(lead, line)
   local output_lines = {}
 
@@ -16,6 +48,11 @@ local function gh_completion(lead, line)
 
   if #args == 0 or lead == '' then
     table.insert(args, '')
+  end
+
+  vim.pretty_print(args)
+  if args[#args] == '--reviewer' or args[#args] == '' and args[#args - 1] == '--reviewer' then
+    return fetch_reviewers()
   end
 
   local job = Job:new({
